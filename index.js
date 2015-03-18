694c4f2a00a6dbef5c363df181351c32eb1ac0de
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-03-18 23:22:23 +0700

    Use spaces.

--- index.js
+++ index.js
@@ -9,76 +9,76 @@ portfinder.basePort = 9222;
 
 function discoverDevices() {
 
-	return adbClient.listDevices()
-		.then(function(devices) {
-			console.log('devices', devices);
+    return adbClient.listDevices()
+        .then(function(devices) {
+            console.log('devices', devices);
 
-			return Promise.reduce(Promise.all(devices.map(findServices)), function(a, b) {
-				a.concat(b);
-			});
-		})
-		.then(function(services) {	
-			console.log('services:', services);	
-			return Promise.all(services.map(setupForward));
-		})
-		.then(function(forwarded) {
-			console.log('forwarded', forwarded);
-			return forwarded;
-		});
+            return Promise.reduce(Promise.all(devices.map(findServices)), function(a, b) {
+                a.concat(b);
+            });
+        })
+        .then(function(services) {  
+            console.log('services:', services); 
+            return Promise.all(services.map(setupForward));
+        })
+        .then(function(forwarded) {
+            console.log('forwarded', forwarded);
+            return forwarded;
+        });
 }
 
 function findServices(device) {
 
-	return adbClient.shell(device.id, 'cat /proc/net/unix | grep devtools_remote')
-   		.then(adb.util.readAll)
-		.then(function(output) {
-			var response = output.toString('utf8');
-			var services = [];
+    return adbClient.shell(device.id, 'cat /proc/net/unix | grep devtools_remote')
+        .then(adb.util.readAll)
+        .then(function(output) {
+            var response = output.toString('utf8');
+            var services = [];
 
-			if(response.length) { 
-				var matches = response.match(/@(.+)/gi);
-				if(matches.length) {
-					services =  matches;
-				}
-			};
+            if(response.length) { 
+                var matches = response.match(/@(.+)/gi);
+                if(matches.length) {
+                    services =  matches;
+                }
+            };
 
-			return Promise.map(services, function(service) {
-				return {
-					device: device,
-					service: service
-				};
-			});
+            return Promise.map(services, function(service) {
+                return {
+                    device: device,
+                    service: service
+                };
+            });
 
-		});
+        });
 }
 
 function setupForward(info) {
 
-	return (getPort().then(function(port) {
-		var localAddress = 'tcp:' + port;
-		var remoteAddress = 'localabstract:' + info.service.replace('@', '');
-		return adbClient
-			.forward(info.device.id, localAddress, remoteAddress)
-			.then(function() {
-				return {
-					device: info.device,
-					url: 'http://localhost:' + port,
-				};
-			});
-	}));
+    return (getPort().then(function(port) {
+        var localAddress = 'tcp:' + port;
+        var remoteAddress = 'localabstract:' + info.service.replace('@', '');
+        return adbClient
+            .forward(info.device.id, localAddress, remoteAddress)
+            .then(function() {
+                return {
+                    device: info.device,
+                    url: 'http://localhost:' + port,
+                };
+            });
+    }));
 
 }
 
 function getPort() {
-	return new Promise(function(resolve, reject) {
-		portfinder.getPort(function(err, port) {
-			if(err) {
-				reject(err);
-			} else {
-				resolve(port);
-			}
-		});
-	});
+    return new Promise(function(resolve, reject) {
+        portfinder.getPort(function(err, port) {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(port);
+            }
+        });
+    });
 }
 
 module.export = discoverDevices;
